f24ef03e81e194b548e49edf2f62c1174666da1a
CMIS <mis.mg2000@gmail.com>
2025-09-01 07:49:30 +0300

    New recap + Material

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    {
-        path: '', loadComponent: () => import('./components/quiz/quiz').then(m => m.Quiz)   
-    },
-    {
-        path: 'results', loadComponent: () => import('./components/results/results').then(m => m.Results)
-    }
+  {
+    path: '',
+    loadComponent: () => import('./components/quiz/quiz').then((m) => m.Quiz),
+  },
+  {
+    path: 'results',
+    loadComponent: () => import('./components/results/results').then((m) => m.Results),
+  },
+  {
+    path: 'recap',
+    loadComponent: () => import('./components/recap/recap').then((m) => m.Recap),
+  },
 ];
--- src/app/components/recap/recap.ts
+++ src/app/components/recap/recap.ts
@@ -1,12 +1,16 @@
 import { Component, inject } from '@angular/core';
 import { Myrec } from '../../services/myrec';
+import { RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-recap',
-  imports: [],
+  imports: [RouterLink, MatButtonModule, MatCardModule],
   templateUrl: './recap.html',
   styleUrl: './recap.css',
 })
 export class Recap {
-  myrecs = inject(Myrec);
+  MyRecs = inject(Myrec);
+  myrecs = this.MyRecs.myrecs();
 }
